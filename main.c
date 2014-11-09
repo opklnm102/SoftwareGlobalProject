0952885395636b502c18b913105ecfe1fd3f561b
princox <rjhoon92@naver.com>
2014-11-09 16:12:53 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,57 @@
-#include<stdio.h>
+{SoftwareGlobalProject
+=====================
+#include <stdio.h>
+#include < windows.h >
 
-int main()
+void gotoxy(int x, int y)
 {
-{
+ COORD Cur;
+ Cur.X=x;
+ Cur.Y=y;
+ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),Cur);
+}
+
+
+int main() {
+
+	int i;
+
+	gotoxy(35,12);
+	printf("*Togedule*");
+
+	for(i=0; i<30; i++){
+		gotoxy(10+i*2, 10);
+		printf("■");
+	}
+
+	for(i=0;i <15; i++){
+		gotoxy(10, 10+i);
+		printf("■");
+	}
+
+	for(i=0;i <30; i++){
+		gotoxy(10+i*2, 25);
+		printf("■");
+	}
+
+	for(i=0;i <15; i++){
+		gotoxy(68, 10+i);
+		printf("■");
+	}
+
+	
+	gotoxy(20, 15);
+	printf("1. 로  그  인");
+	gotoxy(20, 18);
+	printf("2. 회 원 가 입");
+	gotoxy(20, 21);
+	printf("3. 비밀번호 찾기");
+
+	gotoxy(45,23);
+	printf("메뉴선택 :");
+
+	gotoxy(50,50);
+	
+	
+}
+}
